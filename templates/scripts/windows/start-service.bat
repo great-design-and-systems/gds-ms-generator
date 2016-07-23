5d69c8409214be9d93e926d6ef232f71ea8b173f
rickzx98 <rickzx98@gmail.com>
2016-07-24 03:26:15 +0800

    updates

--- templates/scripts/windows/start-service.bat
+++ templates/scripts/windows/start-service.bat
@@ -1,1 +1,1 @@
-docker run -d -v #LOGPATH:/app/log--name=#DOMAIN_SERVICE --link=#DOMAIN_DB:#DOMAIN_DB -p #DOMAIN_SERVICE
+docker run -d -v #LOGPATH:/app/log--name=#DOMAIN_SERVICE --link=#DOMAIN_DB:#DOMAIN_DB #DOMAIN_SERVICE
