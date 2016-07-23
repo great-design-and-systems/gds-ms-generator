bfec9c9c71d19f193efe9e406f3580b25d784054
rickzx98 <rickzx98@gmail.com>
2016-07-24 04:12:30 +0800

    updates

--- templates/scripts/unix/start-service.sh
+++ templates/scripts/unix/start-service.sh
@@ -1,1 +1,1 @@
-docker run -d -v  #LOGPATH:/app/log--name=#DOMAIN_SERVICE --link=#DOMAIN_DB:#DOMAIN_DB #DOMAIN_SERVICE
+docker run -d -v  #LOGPATH:/app/log --name=#DOMAIN_SERVICE --link=#DOMAIN_DB:#DOMAIN_DB #DOMAIN_SERVICE
