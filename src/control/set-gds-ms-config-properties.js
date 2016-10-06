067df10501349c3c6e46b320fcf5f530923a2ad6
rickzx98 <rickzx98@gmail.com>
2016-10-06 08:30:07 +0800

    Merge branch 'gds-assumption-develop' of https://github.com/great-design-and-systems/gds-ms-generator into gds-assumption-develop
    # Conflicts:
    #	gds-ms-config.json

--- src/control/set-gds-ms-config-properties.js
+++ src/control/set-gds-ms-config-properties.js
@@ -1,17 +1,23 @@
 var lodash = require('lodash');
 
 function execute(configFile, callback) {
-    setTimeout(function() {
+    setTimeout(function () {
         if (configFile.properties) {
             var stringifiedJSON = JSON.stringify(configFile);
-            lodash.forEach(configFile.properties, function(value, key) {
+            lodash.forEach(configFile.properties, function (value, key) {
                 var regex = new RegExp('\\${' + key + '}', 'g');
-                lodash.forEach(configFile.properties, function(fieldValue, field) {
+                lodash.forEach(configFile.properties, function (fieldValue, field) {
                     if (key !== field) {
                         value = value.replace(new RegExp('\\${' + field + '}', 'g'), fieldValue);
+                    } else if (field === 'today') {
+                        value = value.replace(new RegExp('\\${' + field + '}', 'g'), new Date().getTime());
                     }
                 });
-                stringifiedJSON = stringifiedJSON.replace(regex, value);
+                if (key === 'today') {
+                    stringifiedJSON = stringifiedJSON.replace(regex, new Date().getTime());
+                } else {
+                    stringifiedJSON = stringifiedJSON.replace(regex, value);
+                }
             });
             callback(undefined, JSON.parse(stringifiedJSON));
         } else {
